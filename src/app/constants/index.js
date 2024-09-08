007f291d40003189375fba5c04604b1450ff9c60
IanFsciety <iandelas123@outlook.com>
2024-09-07 20:40:25 -0400

    translate my informations to pt-br

--- src/app/constants/index.js
+++ src/app/constants/index.js
@@ -1,23 +1,30 @@
 import fincheck from "../../assets/projects/fincheck.jpg";
 import waiterapp from "../../assets/projects/waiterapp.png";
 
-export const HERO_CONTENT = `I am a passionate full stack developer with a knack for crafting robust and scalable web applications. I have honed my skills in front-end technologies like React, React Native and Next.js, as well as back-end technologies like Node.js, Express and Nest.js, PostgreSQL and MongoDB. My goal is to leverage my expertise to create innovative solutions that drive business growth and deliver exceptional user experiences.`;
+export const HERO_CONTENT = `Sou um desenvolvedor full stack apaixonado, com talento para criar aplicações web robustas e escaláveis. Aperfeiçoei minhas habilidades em tecnologias front-end como React, React Native e Next.js, bem como tecnologias back-end como Node.js, Express e Nest.js, PostgreSQL e MongoDB. Meu objetivo é aproveitar minha experiência para criar soluções inovadoras que impulsionem o crescimento dos negócios e proporcionem experiências excepcionais ao usuário.`;
 
-export const ABOUT_TEXT = `I am a dedicated and versatile full stack developer with a passion for creating efficient and user-friendly web applications. I have worked with a variety of technologies, including React, Next.js, Node.js, Nest.js, PostgreSQL, and MongoDB. My journey in web development began with a deep curiosity for how things work, and it has evolved into a career where I continuously strive to learn and adapt to new challenges. I thrive in collaborative environments and enjoy solving complex problems to deliver high-quality solutions. Outside of coding, I enjoy staying active, exploring new technologies.`;
+export const ABOUT_TEXT = `Sou um desenvolvedor full stack dedicado e versátil, apaixonado por criar aplicações web eficientes e fáceis de usar. Trabalhei com diversas tecnologias, incluindo React, Next.js, Node.js, Nest.js, Python, PHP, PostgreSQL e MongoDB. Minha jornada no desenvolvimento web começou com uma profunda curiosidade sobre como as coisas funcionam e evoluiu para uma carreira onde me esforço continuamente para aprender e me adaptar a novos desafios. Eu prospero em ambientes colaborativos e gosto de resolver problemas complexos para entregar soluções de alta qualidade. Fora da programação, gosto de me manter ativo, explorando novas tecnologias.`;
 
 export const EXPERIENCES = [
   {
-    year: "2024 - 2024",
+    year: "2024",
     role: "Full Stack Developer",
-    company: "Personal",
-    description: `Web App designed to control your finances in an easy way, being able to list your transactions with various filters.`,
+    company: "Pessoal",
+    description: `Web App desenvolvido para controlar suas finanças de forma fácil, podendo listar suas transações com diversos filtros.`,
     technologies: ["React / Vite ", "Typescript", "TailwindCSS", "Zod", "React Query","NestJS", "Prisma", "PostgreSQL",],
   },
   {
-    year: "2023 - 2023",
+    year: "2024",
     role: "Full Stack Developer",
     company: "Freelancer",
-    description: `Hybrid application development with React Native and Type Script where I created an app to schedule orders for a relative who sells snacks, which integrated with the API developed with Node TS and WebSockets, with a dashboard that listed the orders made and could be set as ready to pick up the order.`,
+    description: `Fiz com que o sistema de baixas de entrega de uma transportadora fosse automatizado; o responsável pelas baixas escolhia uma lista de Ctes para dar baixa e todos os itens eram baixados automaticamente.`,
+    technologies: ["Python"],
+  },
+  {
+    year: "2023",
+    role: "Full Stack Developer",
+    company: "Freelancer",
+    description: `Desenvolvimento de aplicação híbrida com React Native e Type Script onde criei um app para agendar pedidos para um parente que vende salgadinhos, que integrou com a API desenvolvida com Node TS e WebSockets, com um dashboard que listava os pedidos realizados e podia ser configurado como pronto para retirar o pedido.`,
     technologies: ["React / React Native", "Typescript", "Styled-Components", "NodeJS", "MongoDB","Websockets",],
   },
 ];
@@ -26,15 +33,16 @@ export const PROJECTS = [
   {
     title: "Fincheck Wep App",
     image: fincheck,
+    url: "https://fincheckapp.site",
     description:
-      "Fincheck is an application for personal finance management. Monitor your spending in a simple way, being able to filter by account, category, type of transaction and date.",
+      "Fincheck é um aplicativo para gestão de finanças pessoais. Monitore seus gastos de forma simples, podendo filtrar por conta, categoria, tipo de transação e data.",
     technologies: ["React / Vite ", "Typescript", "TailwindCSS", "Zod", "React Query","NestJS", "Prisma", "PostgreSQL",],
   },
   {
     title: "WaiterApp",
     image: waiterapp,
     description:
-      "WaiterApp is an application for placing and managing orders for a restaurant. The customer or waiter places the customer's order and this order is sent to the kitchen dashboard.",
+      "WaiterApp é um aplicativo para fazer e gerenciar pedidos para um restaurante. O cliente ou garçom faz o pedido do cliente e esse pedido é enviado para o painel da cozinha.",
     technologies: ["React / React Native", "Typescript", "Styled-Components", "NodeJS", "MongoDB","Websockets",],
   },
 ];
